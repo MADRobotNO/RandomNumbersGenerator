7c0d9b9994aa1d01767f695f35aab5f1b67083c0
Martin Agnar Dahl <Martin.Agnar.Dahl@norconsult.com>
2022-12-01 21:50:38 +0100

    Revert "main function done"
    This reverts commit 3c4afce4b63d00645815d898a7a1e30c476817ec.

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-import random
-
-
-def generate_random_numbers(amount, minRange, maxRange):
-    numbers = []
-    for i in range(amount):
-        number = random.randint(minRange, maxRange)
-        while number in numbers:
-            number = random.randint(minRange, maxRange)
-        numbers.append(number)
-    return numbers
 
 
 if __name__ == '__main__':
-    amount = 2
-    minRange = 1
-    maxRange = 10
-
-    random_numbers = generate_random_numbers(amount, minRange, maxRange)
-    print(random_numbers)
+    pass
